ece9ce333f23c054f9859998ccd79b77d18fa2f5
ryder-mcdowell <ryder.mcdowell@gmail.com>
2019-01-08 18:54:39 -0800

    Add user content to home page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './styles.css';
+import firebase from '../Firebase';
 
 class Home extends Component {
   render() {
     return (
       <div className="container">
         <div className="userInfoContainer">
-          <h1>User</h1>
+          <h1>{firebase.auth().currentUser.email}</h1>
         </div>
       </div>
     );
