5f0734fbb205a8e47514063e2e065df48fa65655
ryder-mcdowell <ryder.mcdowell@gmail.com>
2019-01-08 18:22:14 -0800

    Add firebase and signUp/signIn methods

--- src/components/LandingPage/index.js
+++ src/components/LandingPage/index.js
@@ -1,27 +1,50 @@
 import React, { Component } from 'react';
 import './styles.css';
 import { withRouter } from 'react-router-dom';
+import firebase from '../Firebase';
 
 import * as ROUTES from '../../constants/routes';
 
 class LandingPage extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = { username: '', password: '', error: '' };
-  }
+  state = { email: '', password: '', error: '' };
 
   onChange = event => {
     this.setState({ [event.target.name]: event.target.value });
   }
 
-  onSubmit = event => {
-    this.props.history.push(ROUTES.HOME);
+  onSignUpButton(event) {
+    const { email, password } = this.state;
+    event.preventDefault();
+
+    firebase
+    .auth()
+    .createUserWithEmailAndPassword(email, password)
+    .then(user => {
+      this.props.history.push(ROUTES.HOME);
+    })
+    .catch(error => {
+      this.setState({ error });
+    });
+  }
+
+  onSignInButton(event) {
+    const { email, password } = this.state;
+    event.preventDefault();
+
+    firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .then(user => {
+      this.props.history.push(ROUTES.HOME);
+    })
+    .catch(error => {
+      this.setState({ error });
+    });
   }
 
   render() {
     const {
-      username,
+      email,
       password,
       error
     } = this.state;
@@ -29,30 +52,28 @@ class LandingPage extends Component {
     return (
       <div className="container">
         <div className ="authContainer">
-          <form onSubmit={this.onSubmit}>
-            <input
-              name="username"
-              value={username}
-              onChange={this.onChange}
-              type="text"
-              placeholder="username"
-            />
-            <br />
-            <input
-              name="password"
-              value={password}
-              onChange={this.onChange}
-              type="password"
-              placeholder="Password"
-            />
-            <br />
-            <div className="buttonsContainer">
-              <button type="submit">Sign Up</button>
-              <button type="submit">Sign In</button>
-            </div>
+          <input
+            name="email"
+            value={email}
+            onChange={this.onChange}
+            type="text"
+            placeholder="email"
+          />
+          <br />
+          <input
+            name="password"
+            value={password}
+            onChange={this.onChange}
+            type="password"
+            placeholder="Password"
+          />
+          <br />
+          <div className="buttonsContainer">
+            <button onClick={this.onSignUpButton.bind(this)}>Sign Up</button>
+            <button onClick={this.onSignInButton.bind(this)}>Sign In</button>
+          </div>
 
-            {error && <p>{error.message}</p>}
-          </form>
+          {error && <p>{error.message}</p>}
         </div>
       </div>
     );
