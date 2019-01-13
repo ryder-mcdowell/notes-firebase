6b1b5d687408b4acce27eddb0ac854140245f0fe
ryder-mcdowell <ryder.mcdowell@gmail.com>
2019-01-12 22:35:20 -0800

    Add Dashboard master-detail layout/styling with static content

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
 import './styles.css';
-import firebase from '../Firebase';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+
+var data = [{ id: 1, title: 'test1'}, { id: 2, title: 'test2'}, { id: 3, title: 'test3'}];
 
 class Dashboard extends Component {
+  state = { selected: null };
+
   render() {
     return (
       <div className="container">
-        <div className="userInfoContainer">
-          <h1>{firebase.auth().currentUser.email}</h1>
+        <List className="listContainer">
+          {
+            data.map(note =>
+              <ListItem
+                onClick={() => this.setState({ selected: note.id })}
+                selected={this.state.selected === note.id}
+              >
+                <ListItemText>{note.title}</ListItemText>
+              </ListItem>
+            )
+          }
+        </List>
+        <div className="detailContainer">
+          {this.state.selected}
         </div>
       </div>
     );
